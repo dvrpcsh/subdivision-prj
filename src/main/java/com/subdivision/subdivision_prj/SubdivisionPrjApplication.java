7179dbcde73c72abd080448f3b504396d63e7721
Sanghyeop <xoxoxmin1268@naver.com>
2025-08-11 23:33:13 +0900

    소분 프로젝트 생성

---
+++ src/main/java/com/subdivision/subdivision_prj/SubdivisionPrjApplication.java
@@ -0,0 +1,13 @@
+package com.subdivision.subdivision_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubdivisionPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubdivisionPrjApplication.class, args);
+	}
+
+}
